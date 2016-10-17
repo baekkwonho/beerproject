0954c14be2a6e904b93d6a9557301da57aed3cdf
baekkwonho <kwonhobaek@gmail.com>
2016-10-17 16:48:47 +0900

    update by KH

---
+++ src/main/webapp/beermain/beerMainApp.js
@@ -0,0 +1,15 @@
+
+var homeBtn = document.querySelectorAll(".homeBtn")
+for(var i = 0; i <homeBtn.length; i++) {
+	    homeBtn[i].onclick = function(event) {
+	      window.location.href="beerMainApp.html"
+	    }
+	  }
+
+var infoBtn = document.querySelectorAll(".infoBtn")
+for(var i = 0; i <infoBtn.length; i++) {
+		infoBtn[i].onclick = function(event) {
+	      window.location.href="../beer/beerInfoApp.html"
+	    }
+	  }
+
